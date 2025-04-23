692ac1d0ab89bcfa16b31ad48afa683fbc957299
Yuriy Biryukov <yuiry@MacBook-Pro-Yuriy-2.local>
2025-04-23 12:56:27 +0300

    Добавлены кнопки управления стратегией и backend для toggle-enabled и toggle-pause

--- web/web_main.py
+++ web/web_main.py
@@ -3,6 +3,7 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
 import asyncpg
 import redis.asyncio as redis
 import os
